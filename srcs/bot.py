a9e638fac0fb099f9df75f77f29d81976a27972e
gabrielclcardoso <gabrielclcardoso@gmail.com>
2023-12-12 18:35:27 -0300

    feat: initial bot interface

--- srcs/bot.py
+++ srcs/bot.py
@@ -1,11 +1,13 @@
-import telebot
+from telegram import Update
+from telegram.ext import CommandHandler, ContextTypes, Application
+import logging
 import os
 import sys
 
 try:
-    KEY = os.environ['TOKEN']
+    TOKEN = os.environ['TOKEN']
 except KeyError:
-    sys.stderr.write("KEY envrionment variable not set.\n")
+    sys.stderr.write("TOKEN envrionment variable not set.\n")
     exit(1)
 try:
     CHAT_ID = os.environ['CHAT_ID']
@@ -13,6 +15,40 @@ except KeyError:
     sys.stderr.write("CHAT_ID envrionment variable not set.\n")
     exit(1)
 
-bot = telebot.TeleBot(KEY, parse_mode=None)
-bot.send_message(CHAT_ID, "Hello Group")
-bot.infinity_polling()
+
+logging.basicConfig(
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+    level=logging.INFO
+)
+
+
+async def check_nodes(context: ContextTypes.DEFAULT_TYPE):
+    await context.bot.send_message(chat_id=CHAT_ID, text='Checking nodes')
+
+
+async def update_node(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await context.bot.send_message(chat_id=CHAT_ID, text='updating node info')
+
+
+async def add_node(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await context.bot.send_message(chat_id=CHAT_ID, text='adding node')
+
+
+async def del_node(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await context.bot.send_message(chat_id=CHAT_ID, text='deleting node')
+
+
+async def help(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await context.bot.send_message(chat_id=CHAT_ID, text='help message')
+
+application = Application.builder().token(TOKEN).build()
+
+application.add_handler(CommandHandler('update', update_node))
+application.add_handler(CommandHandler('add', add_node))
+application.add_handler(CommandHandler('delete', del_node))
+application.add_handler(CommandHandler('help', help))
+
+job_queue = application.job_queue
+job_queue.run_repeating(check_nodes, interval=3, first=10)
+
+application.run_polling()
